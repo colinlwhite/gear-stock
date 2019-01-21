1dc1034d7272b4792a21df091982ba47e2c06ece
Colin White <clwhite814@gmail.com>
2019-01-20 21:03:17 -0600

    finished with auth and navigation bar

--- src/helpers/data/authRequests.js
+++ src/helpers/data/authRequests.js
@@ -2,6 +2,13 @@ import firebase from 'firebase';
 import 'firebase/auth';
 
 const authenticate = () => {
-  const 
-}
+  const provider = new firebase.auth.GoogleAuthProvider();
+  return firebase.auth().signInWithPopup(provider);
+};
 
+const logoutUser = () => firebase.auth().signOut();
+
+export default {
+  authenticate,
+  logoutUser,
+};
