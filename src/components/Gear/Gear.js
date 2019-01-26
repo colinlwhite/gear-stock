3e011012c9825cff9755e7f40990c21fd983238a
Colin White <clwhite814@gmail.com>
2019-01-26 12:04:47 -0600

    finished with editing / updating

--- src/components/Gear/Gear.js
+++ src/components/Gear/Gear.js
@@ -3,12 +3,13 @@ import GearItem from '../GearItem/GearItem';
 
 class Gear extends React.Component {
   render() {
-    const { gear, deleteSingleGear } = this.props;
+    const { gear, deleteSingleGear, passGearToEdit } = this.props;
     const gearItemComponents = gear.map(gearstock => (
       <GearItem
       gearstock={gearstock}
       key={gearstock.id}
       deleteSingleGear={deleteSingleGear}
+      passGearToEdit={passGearToEdit}
       />
     ));
     return (
--- src/components/GearItem/GearItem.js
+++ src/components/GearItem/GearItem.js
@@ -7,12 +7,19 @@ class GearItem extends React.Component {
     deleteSingleGear(gearstock.id);
   }
 
+  editEvent = (e) => {
+    e.preventDefault();
+    const { passGearToEdit, gearstock } = this.props;
+    passGearToEdit(gearstock.id);
+  }
+
   render() {
     const { gearstock } = this.props;
     return (
       <div>
         <h2>{gearstock.name}</h2>
         <span><button className="btn btn-danger" onClick={this.deleteEvent}>DELETE</button></span>
+        <span><button className="btn btn-primary" onClick={this.editEvent}>EDIT</button></span>
       </div>
     );
   }
