d4d1a40e7173aeed604e1074046729ad7f73ac93
Colin White <clwhite814@gmail.com>
2019-02-11 21:48:56 -0600

    wip

--- src/components/GearItem/GearItem.js
+++ src/components/GearItem/GearItem.js
@@ -28,13 +28,17 @@ class GearItem extends React.Component {
   render() {
     const { gearstock } = this.props;
     return (
-      <div>
+      <div className="container mt-4 d-flex flex-wrap">
+      <div className="col-auto mb-3">
+        <div className="card" style={{ width: '18rem' }}>
         <img to='gear/:id' className="gear-item-image" src={gearstock.img} alt="gear card" onClick={this.singleViewEvent} />
-        <h2>{gearstock.name}</h2>
-        <h2>{formatPrice(gearstock.price)}</h2>
-        <h6>{gearstock.condition}</h6>
-        <span><button className="btn btn-danger" onClick={this.deleteEvent}>DELETE</button></span>
-        <span><Button to='edit/:id' className="btn btn-primary" onClick={this.editEvent}>EDIT</Button></span>
+          <h2>{gearstock.name}</h2>
+          <h2>{formatPrice(gearstock.price)}</h2>
+          <h6>{gearstock.condition}</h6>
+          <span><button className="btn btn-danger" onClick={this.deleteEvent}>DELETE</button></span>
+          <span><Button to='edit/:id' className="btn btn-primary" onClick={this.editEvent}>EDIT</Button></span>
+      </div>
+      </div>
       </div>
     );
   }
