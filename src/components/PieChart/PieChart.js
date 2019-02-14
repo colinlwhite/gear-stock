34713a7933276d55ffbe453163a38cd8edc01ce1
Colin White <clwhite814@gmail.com>
2019-02-13 20:54:14 -0600

    wip

--- src/components/PieChart/PieChart.js
+++ src/components/PieChart/PieChart.js
@@ -52,7 +52,7 @@ for (const chartItem in forTheChart) {
     return (
       <div>
     <PieChart width={800} height={400}>
-        <Pie isAnimationActive={false} data={chartDisplay} cx={200} cy={200} outerRadius={80} fill="#8884d8" label/>
+        <Pie isAnimationActive={false} data={chartDisplay} cx={200} cy={200} outerRadius={80} fill="#BEC0C2" label/>
         {/* <Pie data={data02} cx={500} cy={200} innerRadius={40} outerRadius={80} fill="#82ca9d"/> */}
         <Tooltip/>
       </PieChart>
