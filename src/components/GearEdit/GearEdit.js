c51032513c85ce9b8c1d6fe90296fff0d7b00898
Colin White <clwhite814@gmail.com>
2020-10-28 11:49:24 -0500

    Finished editing the edit pages' form as well

--- src/components/GearEdit/GearEdit.js
+++ src/components/GearEdit/GearEdit.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Input } from 'reactstrap';
+import { Row, Col, Input } from 'reactstrap';
 import gearRequest from '../../helpers/data/gearRequest';
 import authRequests from '../../helpers/data/authRequests';
 import './GearEdit.scss';
@@ -96,33 +96,39 @@ class GearEdit extends React.Component {
     const { newGear } = this.state;
     return (
       <div className="listing-form col">
-      <h1>Edit Gear</h1>
-      <form onSubmit={this.formSubmit} autocomplete="off">
-        <div className="form-group">
-          <label htmlFor="name">Name:</label>
-          <input
-            type="text"
-            className="form-control"
-            id="name"
-            aria-describedby="addressHelp"
-            placeholder="Gear Item"
-            value={newGear.name}
-            onChange={this.nameChange}
-          />
-        </div>
+        <h1>Edit Gear</h1>
+        <form onSubmit={this.formSubmit} autocomplete="off">  
 
-        <div className="form-group">
-          <label htmlFor="manufacturer">Manufacturer:</label>
-          <input
-            type="text"
-            className="form-control"
-            id="manufacturer"
-            aria-describedby="gearManufacturer"
-            placeholder="Manufacturer"
-            value={newGear.manufacturer}
-            onChange={this.manufacturerChange}
-          />
-        </div>
+          <Row>
+            <Col>
+              <div className="form-group">
+                <label htmlFor="name">Name:</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="name"
+                  aria-describedby="gearName"
+                  placeholder="Gear Name"
+                  value={newGear.name}
+                  onChange={this.nameChange}
+                />
+              </div>
+            </Col>
+            <Col>
+              <div className="form-group">
+                <label htmlFor="manufacturer">Manufacturer:</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="manufacturer"
+                  aria-describedby="gearManufacturer"
+                  placeholder="Manufacturer"
+                  value={newGear.manufacturer}
+                  onChange={this.manufacturerChange}
+                />
+              </div>
+            </Col>
+          </Row>
 
         <div className="form-group">
           <label htmlFor="model">Model:</label>
@@ -150,64 +156,73 @@ class GearEdit extends React.Component {
           />
         </div>
 
-        <div className="form-group">
-          <label htmlFor="price">Price:</label>
-          <input
-            type="number"
-            className="form-control"
-            id="price"
-            aria-describedby="gearPrice"
-            placeholder=""
-            value={newGear.price}
-            onChange={this.priceChange}
-          />
-        </div>
-
-<div className="form-group">
-        <label htmlFor="condition">Condition:</label>
-        <Input type="select" onChange={this.conditionChange} value={newGear.condition}>
-                <option>Just Got It</option>
-                <option>Just Like New</option>
-                <option>Excellent</option>
-                <option>Very Good</option>
-                <option>Good</option>
-                <option>Fair</option>
-                <option>Poor</option>
-                <option>Doesn't Work</option>
-              </Input>
-              </div>
-
+          <Row>
+            <Col>
               <div className="form-group">
-        <label htmlFor="categoryDisplay">Category:</label>
-        <Input type="select" onChange={this.categoryDisplayChange} value={newGear.categoryDisplay}>
-                <option>Acoustic Guitars</option>
-                <option>Amps</option>
-                <option>Band and Orchestra</option>
-                <option>Bass Guitars</option>
-                <option>DJ and Lighting</option>
-                <option>Drums & Percussion</option>
-                <option>Effects and Pedals</option>
-                <option>Electric Guitars</option>
-                <option>Home Audio</option>
-                <option>Keyboards and Synths</option>
-                <option>Pro Audio</option>
-              </Input>
+                <label htmlFor="price">Price:</label>
+                <input
+                  type="number"
+                  className="form-control"
+                  id="price"
+                  aria-describedby="gearPrice"
+                  placeholder=""
+                  value={newGear.price}
+                  onChange={this.priceChange}
+                />
               </div>
-
+            </Col>
+            <Col>
               <div className="form-group">
-        <label htmlFor="year">Year:</label>
-        <Input type="select" onChange={this.yearChange} value={newGear.year}>
-                <option>2010s</option>
-                <option>mid-2000s</option>
-                <option>1990s</option>
-                <option>1980s</option>
-                <option>1970s</option>
-                <option>1960s</option>
-                <option>1950s</option>
-                <option>1940s</option>
-                <option>1930s</option>
-              </Input>
+                <label htmlFor="condition">Condition:</label>
+                <Input type="select" onChange={this.conditionChange}>
+                  <option>Just Got It</option>
+                  <option>Just Like New</option>
+                  <option>Excellent</option>
+                  <option>Very Good</option>
+                  <option>Good</option>
+                  <option>Fair</option>
+                  <option>Poor</option>
+                  <option>Doesn't Work</option>
+                </Input>
               </div>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <div className="form-group">
+                <label htmlFor="categoryDisplay">Category:</label>
+                <Input type="select" onChange={this.categoryDisplayChange}>
+                  <option>Acoustic Guitars</option>
+                  <option>Amps</option>
+                  <option>Band and Orchestra</option>
+                  <option>Bass Guitars</option>
+                  <option>DJ and Lighting</option>
+                  <option>Drums & Percussion</option>
+                  <option>Effects and Pedals</option>
+                  <option>Electric Guitars</option>
+                  <option>Home Audio</option>
+                  <option>Keyboards and Synths</option>
+                  <option>Pro Audio</option>
+                </Input>
+              </div>
+            </Col>
+            <Col>
+              <div className="form-group">
+                <label htmlFor="year">Year:</label>
+                <Input type="select" onChange={this.yearChange}>
+                  <option>2010s</option>
+                  <option>mid-2000s</option>
+                  <option>1990s</option>
+                  <option>1980s</option>
+                  <option>1970s</option>
+                  <option>1960s</option>
+                  <option>1950s</option>
+                  <option>1940s</option>
+                  <option>1930s</option>
+                </Input>
+              </div>
+            </Col>
+          </Row>
 
         <div className="form-group">
           <label htmlFor="notes">Notes:</label>
